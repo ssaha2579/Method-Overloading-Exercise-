abc06a661c9355c4ba634a8a78a0d4c3416ab9a5
Sagar Saha <Sagarsaha2579@gmail.com>
2025-09-17 22:41:58 -0400

    initial commit

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -2,9 +2,39 @@
 {
     public class Program
     {
+        public  static int Add(int numOne, int numTwo)
+        { 
+            return numOne + numTwo;
+        }
+
+        public static string Add(int numOne, int numTwo, bool isCurrency)
+        {
+            var sum = numOne + numTwo;
+
+            if (isCurrency && sum > 1)
+            {
+                return $"{sum} dollars";
+            }
+
+            else if (isCurrency && sum == 1)
+            { 
+                    return $"{sum} dollar"; 
+            }
+            else
+            {
+                return sum.ToString();
+            }
+
+        }
+
+        public static string Add(string numOne, string numTwo)
+        {
+            return numOne + numTwo;
+        } 
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine(Add(numOne: 0, numTwo: 0, true));
         }
     }
 }
